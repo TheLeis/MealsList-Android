fc1eee071bd98477fdf4ef162ba5e6ed68ced346
TheLeis <spikyman333@gmail.com>
2025-10-22 13:52:57 +0200

    Started work on favorites activity

---
+++ app/src/main/java/com/ianleis/mealslist_android/ui/home/FavoritesActivity.kt
@@ -0,0 +1,14 @@
+package com.ianleis.mealslist_android.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ianleis.mealslist_android.R
+
+class FavoritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
